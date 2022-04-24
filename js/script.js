ff7875bde4ed28231c9ffdcc38ab764cb34f8cbe
Thapelo Mmakola <mthapelothapza@gmail.com>
2022-04-24 22:25:19 +0200

    added a map API with mapbox

--- js/script.js
+++ js/script.js
@@ -21,3 +21,40 @@ themeToggler.onclick = () =>{
         document.body.classList.remove('active');
     }
 }
+
+// Map
+
+mapboxgl.accessToken =
+  "pk.eyJ1IjoidGhhcGVsbzAyMDIyIiwiYSI6ImNsMmRudXRrbTExMW8zZHA5MXkyNDUwcDIifQ.deTo5BCTRYzqN47P0ZRpJA"
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true
+})
+
+function successLocation(position) {
+  setupMap([position.coords.longitude, position.coords.latitude])
+}
+
+function errorLocation() {
+  setupMap([-2.24, 53.48])
+}
+
+function setupMap(center) {
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: center,
+    zoom: 14
+  })
+
+  const nav = new mapboxgl.NavigationControl()
+  map.addControl(nav)
+
+  var directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken
+  })
+
+//   map.addControl(directions, "top-left")
+}
+
+
